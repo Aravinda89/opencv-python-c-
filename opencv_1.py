d1b4dd2ebf67617fa07b5ab46600c76c25001006
Gayan <1513223865@qq.com>
2022-08-08 17:38:26 +0800

    Create opencv_1.py

---
+++ opencv_1.py
@@ -0,0 +1,20 @@
+# Read, Display and Write an Image using OpenCV
+
+import cv2
+
+# The function cv2.imread() is used to read an image.
+img_grayscale = cv2.imread(r'C:\Users\yga\Desktop\C015005282021802884\test_image.jpg',0)
+
+# The function cv2.imshow() is used to display an image in a window.
+cv2.imshow('graycsale image',img_grayscale)
+
+# waitKey() waits for a key press to close the window and 0 specifies indefinite loop
+cv2.waitKey(0)
+
+# cv2.destroyAllWindows() simply destroys all the windows we created.
+cv2.destroyAllWindows()
+
+# The function cv2.imwrite() is used to write an image.
+cv2.imwrite(r'C:\Users\yga\Desktop\grayscale1.jpg', img_grayscale)
+
+print('Done')

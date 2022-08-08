dcf8cb226acbbaad0341472ce7320b0449310b75
Gayan <1513223865@qq.com>
2022-08-08 17:48:08 +0800

    Update opencv_1.py

--- opencv_1.py
+++ opencv_1.py
@@ -3,11 +3,22 @@
 import cv2
 
 # The function cv2.imread() is used to read an image.
-img_grayscale = cv2.imread(r'C:\Users\yga\Desktop\C015005282021802884\test_image.jpg',0)
+img_path = r'C:\Users\yga\Desktop\test_image.jpg'
+
+img_color = cv2.imread(img_path,1)
+img_grayscale = cv2.imread(img_path,0)
+img_unchanged = cv2.imread(img_path,-1)
+
+# cv2.IMREAD_UNCHANGED  or -1
+# cv2.IMREAD_GRAYSCALE  or 0
+# cv2.IMREAD_COLOR  or 1
 
 # The function cv2.imshow() is used to display an image in a window.
-cv2.imshow('graycsale image',img_grayscale)
-
+#Displays image inside a window
+cv2.imshow('color image',img_color)  
+cv2.imshow('grayscale image',img_grayscale)
+cv2.imshow('unchanged image',img_unchanged)
+	 
 # waitKey() waits for a key press to close the window and 0 specifies indefinite loop
 cv2.waitKey(0)
 
